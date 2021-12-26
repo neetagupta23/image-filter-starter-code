40b8df2633c365653d80f65f9b0fe2bc6b0a1cc8
neetagupta23 <neetuvrma@gmail.com>
2021-12-26 19:38:54 +0530

    commit on 'Dev'

--- src/util/util.ts
+++ src/util/util.ts
@@ -12,7 +12,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string>  {
   return new Promise(async (resolve, reject) => {
     try {
       const photo = await Jimp.read(inputURL);
-      const outpath =
+      const outpath: string =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
         .resize(256, 256) // resize
@@ -21,7 +21,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string>  {
         .write(__dirname + outpath, (img) => {
           resolve(__dirname + outpath);
         });
-    } catch (error) {
+    } catch (error ) {
       reject(error);
     }
   });
